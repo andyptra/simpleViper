dc5270fea676f2a99a92b7c92d7551085ccd338a
= <andyptra96@gmail.com>
2022-07-06 13:06:59 +0700

    Initial Commit

---
+++ DemoApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoApps
+//
+//  Created by OwlNight on 06/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
